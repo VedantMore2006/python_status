d0e54d370ada893e16ecf243dad2cb11a88b5cb0
VedantMore2006 <vedantmoremain@gmail.com>
2025-08-18 20:19:58 +0530

    Day 3

---
+++ exercises/area_calc.py
@@ -0,0 +1,10 @@
+def canno(width, height):
+    can = (width * height) / 5
+    print(can)
+    can = round((width * height) / 5)
+    print(can)
+
+
+w = int(input("Width "))
+h = int(input("height "))
+print(f"no of cans = {canno(w, h)}")
