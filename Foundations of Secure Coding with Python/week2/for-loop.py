00a1816c0b55e07d80dd4eada61d789e64b77cc8
VedantMore2006 <vedantmoremain@gmail.com>
2025-10-13 14:09:35 +0530

    week 2

---
+++ Foundations of Secure Coding with Python/week2/for-loop.py
@@ -0,0 +1,3 @@
+for i in range(0, 10):
+    d = i + 1
+    print(f"{d}. My goal is to become and AI Engineer who intigrates AI to the different applications and builds Agentic AIs")
---
+++ Foundations of Secure Coding with Python/week2/lists.py
@@ -0,0 +1,2 @@
+# to make such program that will store the names of the students and will display their marks in the output
+# and this had to be dynamic 
