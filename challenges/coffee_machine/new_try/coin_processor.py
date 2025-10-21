e0d701d3b2a86e87e0c9a588997b3c7f0fad32ed
VedantMore2006 <vedantmoremain@gmail.com>
2025-10-21 13:26:33 +0530

    still need to work

--- challenges/coffee_machine/new_try/coin_processor.py
+++ challenges/coffee_machine/new_try/coin_processor.py
@@ -1,22 +1,24 @@
 import resources as rs
-pennies = 0.01
-nickels = 0.05
-dimes = 0.1
-quarter = 0.25
-needed_amount = 2.5
-total_amount= 0
-no_of_pennies = int(input("How many pennies do u have : "))
-for i in range(0, no_of_pennies) :
-    total_amount += pennies
-no_of_nickels = int(input("How many nickels do u have : "))
-for i in range(0, no_of_nickels) :
-    total_amount += nickels
-no_of_dimes = int(input("How many demis do u have : "))
-for i in range(0, no_of_dimes) :
-    total_amount += dimes
-no_of_quarters = int(input("How many quarters do u have : "))
-for i in range(0, no_of_quarters) :
-    total_amount += quarter
+# pennies = 0.01
+# nickels = 0.05
+# dimes = 0.1
+# quarter = 0.25
+# needed_amount = 2.5
+# total_amount= 0
+# no_of_pennies = int(input("How many pennies do u have : "))
+# for i in range(0, no_of_pennies) :
+#     total_amount += pennies
+# no_of_nickels = int(input("How many nickels do u have : "))
+# for i in range(0, no_of_nickels) :
+#     total_amount += nickels
+# no_of_dimes = int(input("How many demis do u have : "))
+# for i in range(0, no_of_dimes) :
+#     total_amount += dimes
+# no_of_quarters = int(input("How many quarters do u have : "))
+# for i in range(0, no_of_quarters) :
+#     total_amount += quarter
+
+
 
 if total_amount > needed_amount :
     change = total_amount - needed_amount
