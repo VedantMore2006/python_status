c8ae95764ea719c1b8201357885089cde876a7f4
VedantMore2006 <vedantmoremain@gmail.com>
2025-10-21 11:02:10 +0530

    to do

---
+++ challenges/coffee_machine/process_coins.py
@@ -0,0 +1,12 @@
+pennies = 0.01
+nickels = 0.05
+dimes = 0.1
+quarter = 0.25
+
+no_of_pennies = int(input("How many pennies do u have : "))
+no_of_nickels = int(input("How many nickels do u have : "))
+no_of_dimes = int(input("How many demis do u have : "))
+no_of_quarters = int(input("How many quarters do u have : "))
+print(no_of_dimes)
+print(no_of_nickels)
+print()
