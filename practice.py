da8747a940b800f58df73e866630b9e5b76da844
VedantMore2006 <vedantmoremain@gmail.com>
2025-11-08 16:13:07 +0530

    practice

---
+++ practice.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} year old")
+
+
+student = Student("Alice", 10)
+student.greet()
