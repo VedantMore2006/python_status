e96389eb3196bded1eb2aaf02d4567173abfaa97
VedantMore2006 <vedantmoremain@gmail.com>
2025-11-23 18:23:31 +0530

    question 2 solved

--- DSA/1.max_and_its_index.py
+++ DSA/1.max_and_its_index.py
@@ -7,7 +7,7 @@ for i in arr:
     index += 1
     if max < i:
         max = i
-        max_index = index
+        max_index = index - 1
 
-if arr[max_index - 1] == max:
-    print(f"The max number in the list is {max} and its index is {max_index - 1}")
+if arr[max_index] == max:
+    print(f"The max number in the list is {max} and its index is {max_index}")
